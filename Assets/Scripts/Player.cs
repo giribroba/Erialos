00aa8ae42d84fa4ead22387ed66e7d6e5dae66a9
giribroba <giribroba>
2020-05-20 17:18:19 -0300

    transforma

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float vel, cdDashP, tempoDash;
+    [SerializeField] private Animator animCoracao, animDogao;
     [SerializeField] private Transform dash;
     [SerializeField] private Vector2 fimDaTela;
-    [SerializeField] private GameObject espada;
-    [SerializeField] private SpriteRenderer rendererPlayer;
-    private float cdDashA;
-    private bool mover;
+    [SerializeField] private GameObject espada, spritePlayer, spriteDogao;
+    [SerializeField] private SpriteRenderer rendererPlayer, rendererDogao;
+    [SerializeField] private Image cardio;
+    private float cdDashA, batimentos = 100;
+    private bool bDash, dogao;
     private Rigidbody2D rigPlayer;
     private Animator animPlayer;
     private Vector2 movimento, pMouse;
@@ -22,14 +26,35 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        rendererPlayer.flipX = movimento.x != 0 ? movimento.x < 0 : rendererPlayer.flipX;
-        animPlayer.SetBool("Andando", movimento != Vector2.zero);
+        spriteDogao.SetActive(dogao);
+        spritePlayer.SetActive(!dogao);
+        espada.SetActive(!dogao);
+
+        if (batimentos == 100)
+            dogao = true;
+        else if (batimentos == 20)
+            dogao = false;
+
+        batimentos = Mathf.Clamp(batimentos - Time.deltaTime * 3, 20, 100);
+        cardio.fillAmount = batimentos / 100;
+        animCoracao.speed = batimentos / 100;
         cdDashA -= Time.deltaTime;
 
-        if (Input.GetMouseButtonDown(0) && cdDashA <= 0)
+        if(dogao)
         {
-            mover = false;
-            cdDashA = cdDashP;
+            rendererDogao.flipX = movimento.x != 0 ? movimento.x < 0 : rendererDogao.flipX;
+            animDogao.SetBool("Andando", movimento != Vector2.zero);
+        }
+        else
+        {
+            rendererPlayer.flipX = movimento.x != 0 ? movimento.x < 0 : rendererPlayer.flipX;
+            animPlayer.SetBool("Andando", movimento != Vector2.zero);
+
+            if (Input.GetMouseButtonDown(0) && cdDashA <= 0)
+            {
+                bDash = true;
+                cdDashA = cdDashP;
+            }
         }
 
         if (Mathf.Abs(this.transform.position.x) > fimDaTela.x)
@@ -54,15 +79,16 @@ public class Player : MonoBehaviour
                 this.transform.position = new Vector2(this.transform.position.x, fimDaTela.y - 0.1f);
             }
         }
+
         movimento = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         pMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
-        if (mover)
+        if (!bDash)
         {
-            rigPlayer.velocity = movimento.normalized * vel;
+            rigPlayer.velocity = movimento.normalized * (dogao? vel * 2 : vel);
             espada.transform.up = new Vector2(pMouse.x - this.transform.position.x, pMouse.y - this.transform.position.y);
         }
         else
@@ -75,6 +101,6 @@ public class Player : MonoBehaviour
     {
         this.transform.position = Vector2.MoveTowards(this.transform.position, dash.position, vel/10);
         yield return new WaitForSeconds(tempoDash);
-        mover = true;
+        bDash = false;
     }
 }
